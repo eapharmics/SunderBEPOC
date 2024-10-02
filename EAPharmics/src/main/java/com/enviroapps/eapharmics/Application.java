b17695eaa9598c8142de2b75f683123c8ba84108
anchulamsc <anchu.venki@gmail.com>
2024-10-02 12:42:28 -0500

    code cleanup

--- EAPharmics/src/main/java/com/enviroapps/eapharmics/Application.java
+++ EAPharmics/src/main/java/com/enviroapps/eapharmics/Application.java
@@ -2,10 +2,8 @@ package com.enviroapps.eapharmics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.enviroapps.eapharmics" })
 public class Application {
 
 	public static void main(String[] args) {
